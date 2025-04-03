806fd303d00b0bdbc2b4df65090f5dcc4ee5f5ff
St80ene <etienejames5@gmail.com>
2025-04-03 05:31:06 +0100

    formatting
    Signed-off-by: St80ene <etienejames5@gmail.com>

--- src/auth/role-guard.ts
+++ src/auth/role-guard.ts
@@ -1,7 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { AuthGuard } from '@nestjs/passport';
 import { UserRole } from 'src/users/enums/user-role.enum';
+import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
 
 @Injectable()
 export class RolesGuard extends AuthGuard('jwt') implements CanActivate {
@@ -10,10 +10,8 @@ export class RolesGuard extends AuthGuard('jwt') implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    // Call AuthGuard to ensure the user is authenticated
     const baseGuardResult = await super.canActivate(context);
     if (!baseGuardResult) {
-      // If authentication fails, deny access
       return false;
     }
 
@@ -23,15 +21,12 @@ export class RolesGuard extends AuthGuard('jwt') implements CanActivate {
       context.getClass(),
     ]);
 
-    // If no roles are required, allow access
     if (!requireRoles) {
       return true;
     }
 
-    // Get the user from the request (already injected by the AuthGuard)
     const { user } = context.switchToHttp().getRequest();
 
-    // Check if the user's roles (from DB) match the required roles
     return requireRoles.some((role) => user.roles?.includes(role));
   }
 }
