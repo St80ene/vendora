0f5231f73deaf219b3d88bb15deb24828268f0b3
St80ene <etienejames5@gmail.com>
2025-04-04 06:39:57 +0100

    Setup Swagger API documentation
    Signed-off-by: St80ene <etienejames5@gmail.com>

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,6 +15,29 @@ export class AppController {
     private readonly authService: AuthService
   ) {}
 
+  /** GET Methods */
+  /**
+   * @openapi
+   * '/api/user/{username}':
+   *  get:
+   *     tags:
+   *     - User Controller
+   *     summary: Get a user by username
+   *     parameters:
+   *      - name: username
+   *        in: path
+   *        description: The username of the user
+   *        required: true
+   *     responses:
+   *      200:
+   *        description: Fetched Successfully
+   *      400:
+   *        description: Bad Request
+   *      404:
+   *        description: Not Found
+   *      500:
+   *        description: Server Error
+   */
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './utils/exceptions/global-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const PORT = process.env.PORT || 5000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Vendora')
+    .setDescription('The Vendora API description')
+    .setVersion('1.0')
+    .addTag('e-commerce')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   app.enableCors({
     origin: '*',
